6bb72c7be66e93d625dec438147376946d78169c
srivera-everfi <srivera@everfi.com>
2018-10-25 23:05:00 -0400

    building

---
+++ docs/src/index.js
@@ -0,0 +1,54 @@
+import _ from 'lodash';
+import Chart from 'chart.js';
+import './styles/main.scss';
+
+function component() {
+  let element = document.createElement('div');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  return element;
+}
+
+document.body.appendChild(component());
+
+var ctx = document.getElementById("myChart");
+var myChart = new Chart(ctx, {
+  type: 'horizontalBar',
+  responsive: 'true',
+  data: {
+    labels: ["SASS/Less", "JavaScript ES6", "React", "Angular 6", "BackboneJS", "NodeJS"],
+    datasets: [{
+      label: 'Experience',
+      data: [100, 90, 80, 60, 50, 30],
+      backgroundColor: [
+        '#6c468e',
+        '#e16479',
+        '#e09535',
+        '#10b798',
+        '#0086ae',
+        '#005198'
+      ]
+    }]
+  },
+  options: {
+    legend: {
+      display: false
+    },
+    layout: {
+      padding: {
+        left: 50,
+        right: 0,
+        top: 0,
+        bottom: 0
+      }
+    },
+    maintainAspectRatio: false,
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+});
